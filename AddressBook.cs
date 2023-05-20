84bd39ff736ffdb3c868749f7c2344fd9b0cfd05
Mahesh <Maheshk2104@gmail.com>
2023-05-20 17:50:14 +0530

    UC3

--- AddressBook.cs
+++ AddressBook.cs
@@ -20,6 +20,46 @@ namespace AddressBookSystem
             contacts.Add(contact);
         }
 
+        public void EditContact(string firstName, string lastName)
+        {
+            Contact contact = FindContact(firstName, lastName);
+
+            if (contact != null)
+            {
+                Console.WriteLine("Enter the updated details for the contact:");
+
+                Console.Write("First Name: ");
+                contact.FirstName = Console.ReadLine();
+
+                Console.Write("Last Name: ");
+                contact.LastName = Console.ReadLine();
+
+                Console.Write("Address: ");
+                contact.Address = Console.ReadLine();
+
+                Console.Write("City: ");
+                contact.City = Console.ReadLine();
+
+                Console.Write("State: ");
+                contact.State = Console.ReadLine();
+
+                Console.Write("ZIP Code: ");
+                contact.Zip = Console.ReadLine();
+
+                Console.Write("Phone Number: ");
+                contact.PhoneNumber = Console.ReadLine();
+
+                Console.Write("Email: ");
+                contact.Email = Console.ReadLine();
+
+                Console.WriteLine("Contact updated successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found!");
+            }
+        }
+
         public void DisplayContacts()
         {
             foreach (var contact in contacts)
@@ -29,11 +69,17 @@ namespace AddressBookSystem
                 Console.WriteLine("Address: " + contact.Address);
                 Console.WriteLine("City: " + contact.City);
                 Console.WriteLine("State: " + contact.State);
-                Console.WriteLine("Zip: " + contact.Zip);
+                Console.WriteLine("ZIP Code: " + contact.Zip);
                 Console.WriteLine("Phone Number: " + contact.PhoneNumber);
                 Console.WriteLine("Email: " + contact.Email);
                 Console.WriteLine();
             }
         }
+
+        private Contact FindContact(string firstName, string lastName)
+        {
+            return contacts.Find(c => c.FirstName == firstName && c.LastName == lastName);
+        }
     }
+
 }
