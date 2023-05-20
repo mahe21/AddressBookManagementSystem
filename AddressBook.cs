be6fa688544548b4b12f4c306202677e2000872d
Mahesh <Maheshk2104@gmail.com>
2023-05-20 17:39:55 +0530

    UC2

---
+++ AddressBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine("First Name: " + contact.FirstName);
+                Console.WriteLine("Last Name: " + contact.LastName);
+                Console.WriteLine("Address: " + contact.Address);
+                Console.WriteLine("City: " + contact.City);
+                Console.WriteLine("State: " + contact.State);
+                Console.WriteLine("Zip: " + contact.Zip);
+                Console.WriteLine("Phone Number: " + contact.PhoneNumber);
+                Console.WriteLine("Email: " + contact.Email);
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,15 +6,67 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Contact newContact = new Contact();
-            newContact.FirstName = "John";
-            newContact.LastName = "Doe";
-            newContact.Address = "123 Main Street";
-            newContact.City = "Anytown";
-            newContact.State = "California";
-            newContact.Zip = "12345";
-            newContact.PhoneNumber = "(555) 123-4567";
-            newContact.Email = "johndoe@example.com";
+            AddressBook addressBook = new AddressBook();
+
+            Console.WriteLine("=== Address Book ===");
+
+            while (true)
+            {
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display Contacts");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Contact newContact = new Contact();
+
+                        Console.Write("First Name: ");
+                        newContact.FirstName = Console.ReadLine();
+
+                        Console.Write("Last Name: ");
+                        newContact.LastName = Console.ReadLine();
+
+                        Console.Write("Address: ");
+                        newContact.Address = Console.ReadLine();
+
+                        Console.Write("City: ");
+                        newContact.City = Console.ReadLine();
+
+                        Console.Write("State: ");
+                        newContact.State = Console.ReadLine();
+
+                        Console.Write("ZIP Code: ");
+                        newContact.Zip = Console.ReadLine();
+
+                        Console.Write("Phone Number: ");
+                        newContact.PhoneNumber = Console.ReadLine();
+
+                        Console.Write("Email: ");
+                        newContact.Email = Console.ReadLine();
+
+                        addressBook.AddContact(newContact);
+
+                        Console.WriteLine("Contact added successfully!");
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("=== Contacts ===");
+                        addressBook.DisplayContacts();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
         }
     }
 }
