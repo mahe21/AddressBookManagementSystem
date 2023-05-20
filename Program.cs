eecb83bf4c5278b9cbb205e39b38f52ba766e3f2
Mahesh <Maheshk2104@gmail.com>
2023-05-20 17:34:13 +0530

    UC1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact newContact = new Contact();
+            newContact.FirstName = "John";
+            newContact.LastName = "Doe";
+            newContact.Address = "123 Main Street";
+            newContact.City = "Anytown";
+            newContact.State = "California";
+            newContact.Zip = "12345";
+            newContact.PhoneNumber = "(555) 123-4567";
+            newContact.Email = "johndoe@example.com";
+        }
+    }
+}
